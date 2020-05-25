45e62ebad3a2073ec42854f43faa0bed7eaa48a3
Trong Nhan <trongnhannguyen94@gmail.com>
2020-05-25 17:56:16 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.contaier}>
+      <Text>Name: Test Deploy GitHub</Text>
+      <Text>Author: Nguyen Trong Nhan</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  contaier: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+});
